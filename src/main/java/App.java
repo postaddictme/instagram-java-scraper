169f3c17671941c41bb35a6ad1e104b42be85271
raiym <r.kapishev@gmail.com>
2016-07-05 18:24:31 +0300

    Change package name

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,6 @@
-import me.postaddict.instagram.Instagram;
-import me.postaddict.instagram.exception.InstagramException;
-import me.postaddict.instagram.model.Media;
+import me.postaddict.instagramscraper.Instagram;
+import me.postaddict.instagramscraper.exception.InstagramException;
+import me.postaddict.instagramscraper.model.Media;
 
 import java.io.IOException;
 
@@ -11,11 +11,11 @@ public class App {
     public static void main(String[] args) {
         Instagram instagram = new Instagram();
         try {
-            me.postaddict.instagram.model.Account account = instagram.getAccount("durov");
+            me.postaddict.instagramscraper.model.Account account = instagram.getAccount("kevin");
             System.out.println(account.mediaCount);
-//            List<Media> medias = instagram.getMedias("durov", 12);
+//            List<Media> medias = instagramscraper.getMedias("durov", 12);
 //            System.out.println(medias.get(0).imageHighResolutionUrl);
-            Media media = instagram.getMediaByUrl("https://www.instagram.com/p/BGY0zB4r7X2/");
+            Media media = instagram.getMediaByUrl("https://www.instagramscraper.com/p/BGY0zB4r7X2/");
             System.out.println(media.owner.username);
         } catch (IOException e) {
             e.printStackTrace();
