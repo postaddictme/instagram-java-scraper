136cbce2f4851c1566dcf73252cface981f50253
Vasily Rozhnikov <shiawasena.hoshi@gmail.com>
2016-12-09 20:40:41 +0300

    - add three constructors for more flexibility (particularly for scrapping via proxy)

--- src/test/java/InstagramTest.java
+++ src/test/java/InstagramTest.java
@@ -3,6 +3,7 @@ import me.postaddict.instagramscraper.exception.InstagramException;
 import me.postaddict.instagramscraper.model.Account;
 import me.postaddict.instagramscraper.model.Comment;
 import me.postaddict.instagramscraper.model.Media;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -11,52 +12,51 @@ import java.util.List;
 import static org.junit.Assert.assertEquals;
 
 public class InstagramTest {
+    Instagram instagram;
+
+    @Before
+    public void setUp() throws Exception {
+        instagram = new Instagram();
+    }
 
     @Test
     public void testGetAccountByUsername() throws IOException, InstagramException {
-        Instagram instagram = new Instagram();
         Account account = instagram.getAccountByUsername("kevin");
         assertEquals("kevin", account.username);
     }
 
     @Test
     public void testGetAccountById() throws IOException, InstagramException {
-        Instagram instagram = new Instagram();
         Account account = instagram.getAccountById(3);
         assertEquals("kevin", account.username);
     }
-    
+
     @Test
     public void testGetMedias() throws IOException, InstagramException {
-        Instagram instagram = new Instagram();
         List<Media> mediaList = instagram.getMedias("kevin", 50);
         assertEquals(50, mediaList.size());
     }
-    
+
     @Test
     public void testGetMediaByUrl() throws IOException, InstagramException {
-        Instagram instagram = new Instagram();
         Media media = instagram.getMediaByUrl("https://www.instagram.com/p/BHaRdodBouH");
         assertEquals("kevin", media.owner.username);
     }
-    
+
     @Test
     public void testGetMediaByCode() throws IOException, InstagramException {
-        Instagram instagram = new Instagram();
         Media media = instagram.getMediaByCode("BHaRdodBouH");
         assertEquals("kevin", media.owner.username);
     }
 
     @Test
     public void testGetLocationMediasById() throws IOException, InstagramException {
-        Instagram instagram = new Instagram();
         List<Media> list = instagram.getLocationMediasById("17326249", 13);
         assertEquals(13, list.size());
     }
 
     @Test
     public void testGetCommentsByMediaCode() throws Exception {
-        Instagram instagram = new Instagram();
         List<Comment> list = instagram.getCommentsByMediaCode("BHaRdodBouH", 13);
         assertEquals(13, list.size());
         assertEquals("Beautiful dog xx", list.get(0).text);
