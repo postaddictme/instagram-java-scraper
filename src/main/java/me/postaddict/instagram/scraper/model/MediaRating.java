380b07e45c8e8841b6326243f74a415f0fccb829
igor-suhorukov <igor.suhorukov@gmail.com>
2017-11-17 00:40:12 +0300

    data model WIP

--- src/main/java/me/postaddict/instagram/scraper/model/MediaRating.java
+++ src/main/java/me/postaddict/instagram/scraper/model/MediaRating.java
@@ -8,6 +8,6 @@ import java.util.Collection;
 @Data
 @ToString
 public class MediaRating {
-    private PageObject<me.postaddict.instagram.scraper.domain.Media> media;//media": {"nodes"
+    private PageObject<Media> media;//media": {"nodes"
     private Collection<Media> topPosts;//"top_posts": {"nodes": [
 }
