8f6c4d011897adf1a15cdc02c723c122f4dd78f5
igor-suhorukov <igor.suhorukov@gmail.com>
2017-11-25 15:35:45 +0300

    fix issue with Tag jpa mapping

--- src/main/java/me/postaddict/instagram/scraper/model/Tag.java
+++ src/main/java/me/postaddict/instagram/scraper/model/Tag.java
@@ -3,11 +3,13 @@ package me.postaddict.instagram.scraper.model;
 import lombok.Data;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Transient;
 
 @Entity
 @Data
 public class Tag {
+    @Id
     private String name;
     private Integer count;
     @Transient
