b4a3a84cb7a78e7ee685f45c796a01d0f14ec77f
igor-suhorukov <igor.suhorukov@gmail.com>
2017-11-27 01:07:17 +0300

    try to fix metadata issue with JPA mapping and avoid moxy warnings [EL Warning]: 2017-11-27 00:35:04.991--Ignoring attribute [width] on class [me.postaddict.instagram.scraper.model.Media] as no Property was generated for it. [EL Warning]: 2017-11-27 00:35:04.991--Ignoring attribute [height] on class [me.postaddict.instagram.scraper.model.Media] as no Property was generated for it. [EL Warning]: 2017-11-27 00:35:04.991--Ignoring attribute [displayUrl] on class [me.postaddict.instagram.scraper.model.Media] as no Property was generated for it. [EL Warning]: 2017-11-27 00:35:04.991--Ignoring attribute [isVideo] on class [me.postaddict.instagram.scraper.model.Media] as no Property was generated for it. [EL Warning]: 2017-11-27 00:35:04.991--Ignoring attribute [shouldLogClientEvent] on class [me.postaddict.instagram.scraper.model.Media] as no Property was generated for it. [EL Warning]: 2017-11-27 00:35:04.991--Ignoring attribute [trackingToken] on class [me.postaddict.instagram.scraper.model.Media] as no Property was generated for it. [EL Warning]: 2017-11-27 00:35:04.992--Ignoring attribute [displayResources] on class [me.postaddict.instagram.scraper.model.Media] as no Property was generated for it.

--- src/main/java/me/postaddict/instagram/scraper/model/Media.java
+++ src/main/java/me/postaddict/instagram/scraper/model/Media.java
@@ -9,106 +9,51 @@ import java.util.List;
 
 @Entity
 @Data
-public class Media extends MediaResource{
+public class Media {
+    protected Integer height;
+    protected Integer width;
+    protected String displayUrl;
+    @ElementCollection
+    protected List<DisplayResource> displayResources;
+    protected Boolean isVideo;
+    @Transient
+    protected Boolean shouldLogClientEvent;
+    @Transient
+    protected String trackingToken;
     private MediaType mediaType;
+    @Id
     private long id;
     private String shortcode;
+    @Transient
     private String gatingInfo;
+    @Column(name = "caption", length = 4096)
     private String caption;
     private Integer commentCount;
+    @Transient
     private PageObject<Comment> commentPreview;
+    @ManyToMany
     private List<Comment> firstComments;
     private Boolean commentsDisabled;
     private Boolean captionIsEdited;
     private Long takenAtTimestamp;
     private Integer likeCount;
+    @ManyToMany
     private List<Account> firstLikes;
+    @ManyToOne
+    @JoinColumn(name="location_id")
     private Location location;
+    @ManyToOne
+    @JoinColumn(name="owner_id")
     private Account owner;
     private Boolean viewerHasLiked;
     private Boolean viewerHasSaved;
     private Boolean viewerHasSavedToCollection;
     private Boolean isAdvertising;
+    @OneToMany(mappedBy = "parentMedia")
     private Collection<CarouselResource> carouselMedia;
+    @ElementCollection
     private Collection<TaggedUser> taggedUser;
     private Date lastUpdated = new Date();
-
-    @Id
-    public long getId() {
-        return id;
-    }
-
-    @Transient
-    public String getGatingInfo() {
-        return gatingInfo;
-    }
-
-    @Column(name = "caption", length = 4096)
-    public String getCaption() {
-        return caption;
-    }
-
-    @Transient
-    public PageObject<Comment> getCommentPreview() {
-        return commentPreview;
-    }
-
-    @ManyToMany
-    public List<Comment> getFirstComments() {
-        return firstComments;
-    }
-
-    @ManyToMany
-    public List<Account> getFirstLikes() {
-        return firstLikes;
-    }
-
-    @ManyToOne
-    @JoinColumn(name="location_id")
-    public Location getLocation() {
-        return location;
-    }
-
-    @ManyToOne
-    @JoinColumn(name="owner_id")
-    public Account getOwner() {
-        return owner;
-    }
-
-    @OneToMany(mappedBy = "parentMedia")
-    public Collection<CarouselResource> getCarouselMedia() {
-        return carouselMedia;
-    }
-
-    @ElementCollection
-    public Collection<TaggedUser> getTaggedUser() {
-        return taggedUser;
-    }
-
-    @Column(name = "height") @Override
-    public Integer getHeight() {
-        return height;
-    }
-
-    @Column(name = "width") @Override
-    public Integer getWidth() {
-        return width;
-    }
-
-    @Column(name = "display_url") @Override
-    public String getDisplayUrl() {
-        return displayUrl;
-    }
-
-    @ElementCollection @Override
-    public List<DisplayResource> getDisplayResources() {
-        return displayResources;
-    }
-
-    @Column(name = "is_video") @Override
-    public Boolean getIsVideo() {
-        return isVideo;
-    }
 
     @Transient
     public Date getCreated(){
