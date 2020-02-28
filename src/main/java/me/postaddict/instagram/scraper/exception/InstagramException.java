abbbf82c1ca878c4cac62105ea17aa27a7a51f83
cyrus <j07424-development@yahoo.co.jp>
2020-02-28 17:10:50 +0900

    Improve features
    * Fix mapper json.
    * Fix request with CSRFToken.
    * Add is_business_account flag support.
    * Add method for get login user ID.
    * Add request header parameters.
    * Add login request parameter.
    * Add common request header
    * Fix getAccountById method causes NullPointerException.
    * Add error type parameter support on InstagramException.
    * Change dependency library version.

--- src/main/java/me/postaddict/instagram/scraper/exception/InstagramException.java
+++ src/main/java/me/postaddict/instagram/scraper/exception/InstagramException.java
@@ -2,12 +2,26 @@ package me.postaddict.instagram.scraper.exception;
 
 import java.io.IOException;
 
+import me.postaddict.instagram.scraper.ErrorType;
+
 public class InstagramException extends IOException {
+	
+    private ErrorType errorType;
 
     public InstagramException() {
     }
 
-    public InstagramException(String message) {
+    public InstagramException(String message, ErrorType errorType) {
         super(message);
+        this.errorType = errorType;
+    }
+
+    public ErrorType getErrorType() {
+        return errorType;
+    }
+
+    @Override
+    public String getMessage() {
+        return super.getMessage() + " : " + getErrorType();
     }
 }
